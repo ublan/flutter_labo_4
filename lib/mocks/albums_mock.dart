32e373f08713ab6b4128eee2247856a69bb274b0
jauday <jauday@dashsolutions.com>
2024-12-01 12:41:22 -0300

    Auday - Albums module added

---
+++ lib/mocks/albums_mock.dart
@@ -0,0 +1,482 @@
+const List albums = [
+  [
+    1,
+    'Thriller',
+    'Michael Jackson',
+    'thriller.png',
+    '1982',
+    ['Wanna Be Startin\' Somethin\'', 'Thriller', 'Beat It']
+  ],
+  [
+    2,
+    'Abbey Road',
+    'The Beatles',
+    'abbey_road.png',
+    '1969',
+    ['Come Together', 'Something', 'Here Comes the Sun']
+  ],
+  [
+    3,
+    'The Dark Side of the Moon',
+    'Pink Floyd',
+    'dark_side_of_the_moon.png',
+    '1973',
+    ['Money', 'Time', 'The Great Gig in the Sky']
+  ],
+  [
+    4,
+    'Hotel California',
+    'Eagles',
+    'hotel_california.png',
+    '1976',
+    ['Hotel California', 'New Kid in Town', 'Life in the Fast Lane']
+  ],
+  [
+    5,
+    'Rumours',
+    'Fleetwood Mac',
+    'album.png',
+    '1977',
+    ['Dreams', 'Go Your Own Way', 'The Chain']
+  ],
+  [
+    6,
+    'Back in Black',
+    'AC/DC',
+    'album.png',
+    '1980',
+    ['Hells Bells', 'Back in Black', 'You Shook Me All Night Long']
+  ],
+  [
+    7,
+    'The Wall',
+    'Pink Floyd',
+    'album.png',
+    '1979',
+    ['Another Brick in the Wall, Part 2', 'Comfortably Numb', 'Run Like Hell']
+  ],
+  [
+    8,
+    'Sgt. Pepper\'s Lonely Hearts Club Band',
+    'The Beatles',
+    'album.png',
+    '1967',
+    ['Lucy in the Sky with Diamonds', 'A Day in the Life', 'With a Little Help from My Friends']
+  ],
+  [
+    9,
+    'Nevermind',
+    'Nirvana',
+    'album.png',
+    '1991',
+    ['Smells Like Teen Spirit', 'Come as You Are', 'Lithium']
+  ],
+  [
+    10,
+    'Led Zeppelin IV',
+    'Led Zeppelin',
+    'album.png',
+    '1971',
+    ['Stairway to Heaven', 'Black Dog', 'Rock and Roll']
+  ],
+  [
+    11,
+    'Thriller',
+    'Michael Jackson',
+    'thriller.png',
+    '1982',
+    ['Wanna Be Startin\' Somethin\'', 'Thriller', 'Beat It']
+  ],
+  [
+    12,
+    'Abbey Road',
+    'The Beatles',
+    'abbey_road.png',
+    '1969',
+    ['Come Together', 'Something', 'Here Comes the Sun']
+  ],
+  [
+    13,
+    'The Dark Side of the Moon',
+    'Pink Floyd',
+    'dark_side_of_the_moon.png',
+    '1973',
+    ['Money', 'Time', 'The Great Gig in the Sky']
+  ],
+  [
+    14,
+    'Hotel California',
+    'Eagles',
+    'hotel_california.png',
+    '1976',
+    ['Hotel California', 'New Kid in Town', 'Life in the Fast Lane']
+  ],
+  [
+    15,
+    'Rumours',
+    'Fleetwood Mac',
+    'album.png',
+    '1977',
+    ['Dreams', 'Go Your Own Way', 'The Chain']
+  ],
+  [
+    16,
+    'Back in Black',
+    'AC/DC',
+    'album.png',
+    '1980',
+    ['Hells Bells', 'Back in Black', 'You Shook Me All Night Long']
+  ],
+  [
+    17,
+    'The Wall',
+    'Pink Floyd',
+    'album.png',
+    '1979',
+    ['Another Brick in the Wall, Part 2', 'Comfortably Numb', 'Run Like Hell']
+  ],
+  [
+    18,
+    'Sgt. Pepper\'s Lonely Hearts Club Band',
+    'The Beatles',
+    'album.png',
+    '1967',
+    ['Lucy in the Sky with Diamonds', 'A Day in the Life', 'With a Little Help from My Friends']
+  ],
+  [
+    19,
+    'Nevermind',
+    'Nirvana',
+    'album.png',
+    '1991',
+    ['Smells Like Teen Spirit', 'Come as You Are', 'Lithium']
+  ],
+  [
+    20,
+    'Led Zeppelin IV',
+    'Led Zeppelin',
+    'album.png',
+    '1971',
+    ['Stairway to Heaven', 'Black Dog', 'Rock and Roll']
+  ],
+  [
+    21,
+    'Thriller',
+    'Michael Jackson',
+    'thriller.png',
+    '1982',
+    ['Wanna Be Startin\' Somethin\'', 'Thriller', 'Beat It']
+  ],
+  [
+    22,
+    'Abbey Road',
+    'The Beatles',
+    'abbey_road.png',
+    '1969',
+    ['Come Together', 'Something', 'Here Comes the Sun']
+  ],
+  [
+    23,
+    'The Dark Side of the Moon',
+    'Pink Floyd',
+    'dark_side_of_the_moon.png',
+    '1973',
+    ['Money', 'Time', 'The Great Gig in the Sky']
+  ],
+  [
+    24,
+    'Hotel California',
+    'Eagles',
+    'hotel_california.png',
+    '1976',
+    ['Hotel California', 'New Kid in Town', 'Life in the Fast Lane']
+  ],
+  [
+    25,
+    'Rumours',
+    'Fleetwood Mac',
+    'album.png',
+    '1977',
+    ['Dreams', 'Go Your Own Way', 'The Chain']
+  ],
+  [
+    26,
+    'Back in Black',
+    'AC/DC',
+    'album.png',
+    '1980',
+    ['Hells Bells', 'Back in Black', 'You Shook Me All Night Long']
+  ],
+  [
+    27,
+    'The Wall',
+    'Pink Floyd',
+    'album.png',
+    '1979',
+    ['Another Brick in the Wall, Part 2', 'Comfortably Numb', 'Run Like Hell']
+  ],
+  [
+    28,
+    'Sgt. Pepper\'s Lonely Hearts Club Band',
+    'The Beatles',
+    'album.png',
+    '1967',
+    ['Lucy in the Sky with Diamonds', 'A Day in the Life', 'With a Little Help from My Friends']
+  ],
+  [
+    29,
+    'Nevermind',
+    'Nirvana',
+    'album.png',
+    '1991',
+    ['Smells Like Teen Spirit', 'Come as You Are', 'Lithium']
+  ],
+  [
+    30,
+    'Led Zeppelin IV',
+    'Led Zeppelin',
+    'album.png',
+    '1971',
+    ['Stairway to Heaven', 'Black Dog', 'Rock and Roll']
+  ],
+  [
+    31,
+    'Thriller',
+    'Michael Jackson',
+    'thriller.png',
+    '1982',
+    ['Wanna Be Startin\' Somethin\'', 'Thriller', 'Beat It']
+  ],
+  [
+    32,
+    'Abbey Road',
+    'The Beatles',
+    'abbey_road.png',
+    '1969',
+    ['Come Together', 'Something', 'Here Comes the Sun']
+  ],
+  [
+    33,
+    'The Dark Side of the Moon',
+    'Pink Floyd',
+    'dark_side_of_the_moon.png',
+    '1973',
+    ['Money', 'Time', 'The Great Gig in the Sky']
+  ],
+  [
+    34,
+    'Hotel California',
+    'Eagles',
+    'hotel_california.png',
+    '1976',
+    ['Hotel California', 'New Kid in Town', 'Life in the Fast Lane']
+  ],
+  [
+    35,
+    'Rumours',
+    'Fleetwood Mac',
+    'album.png',
+    '1977',
+    ['Dreams', 'Go Your Own Way', 'The Chain']
+  ],
+  [
+    36,
+    'Back in Black',
+    'AC/DC',
+    'album.png',
+    '1980',
+    ['Hells Bells', 'Back in Black', 'You Shook Me All Night Long']
+  ],
+  [
+    37,
+    'The Wall',
+    'Pink Floyd',
+    'album.png',
+    '1979',
+    ['Another Brick in the Wall, Part 2', 'Comfortably Numb', 'Run Like Hell']
+  ],
+  [
+    38,
+    'Sgt. Pepper\'s Lonely Hearts Club Band',
+    'The Beatles',
+    'album.png',
+    '1967',
+    ['Lucy in the Sky with Diamonds', 'A Day in the Life', 'With a Little Help from My Friends']
+  ],
+  [
+    39,
+    'Nevermind',
+    'Nirvana',
+    'album.png',
+    '1991',
+    ['Smells Like Teen Spirit', 'Come as You Are', 'Lithium']
+  ],
+  [
+    40,
+    'Led Zeppelin IV',
+    'Led Zeppelin',
+    'album.png',
+    '1971',
+    ['Stairway to Heaven', 'Black Dog', 'Rock and Roll']
+  ],
+  [
+    41,
+    'Thriller',
+    'Michael Jackson',
+    'thriller.png',
+    '1982',
+    ['Wanna Be Startin\' Somethin\'', 'Thriller', 'Beat It']
+  ],
+  [
+    42,
+    'Abbey Road',
+    'The Beatles',
+    'abbey_road.png',
+    '1969',
+    ['Come Together', 'Something', 'Here Comes the Sun']
+  ],
+  [
+    43,
+    'The Dark Side of the Moon',
+    'Pink Floyd',
+    'dark_side_of_the_moon.png',
+    '1973',
+    ['Money', 'Time', 'The Great Gig in the Sky']
+  ],
+  [
+    44,
+    'Hotel California',
+    'Eagles',
+    'hotel_california.png',
+    '1976',
+    ['Hotel California', 'New Kid in Town', 'Life in the Fast Lane']
+  ],
+  [
+    45,
+    'Rumours',
+    'Fleetwood Mac',
+    'album.png',
+    '1977',
+    ['Dreams', 'Go Your Own Way', 'The Chain']
+  ],
+  [
+    46,
+    'Back in Black',
+    'AC/DC',
+    'album.png',
+    '1980',
+    ['Hells Bells', 'Back in Black', 'You Shook Me All Night Long']
+  ],
+  [
+    47,
+    'The Wall',
+    'Pink Floyd',
+    'album.png',
+    '1979',
+    ['Another Brick in the Wall, Part 2', 'Comfortably Numb', 'Run Like Hell']
+  ],
+  [
+    48,
+    'Sgt. Pepper\'s Lonely Hearts Club Band',
+    'The Beatles',
+    'album.png',
+    '1967',
+    ['Lucy in the Sky with Diamonds', 'A Day in the Life', 'With a Little Help from My Friends']
+  ],
+  [
+    49,
+    'Nevermind',
+    'Nirvana',
+    'album.png',
+    '1991',
+    ['Smells Like Teen Spirit', 'Come as You Are', 'Lithium']
+  ],
+  [
+    50,
+    'Led Zeppelin IV',
+    'Led Zeppelin',
+    'album.png',
+    '1971',
+    ['Stairway to Heaven', 'Black Dog', 'Rock and Roll']
+  ],
+  [
+    51,
+    'Thriller',
+    'Michael Jackson',
+    'thriller.png',
+    '1982',
+    ['Wanna Be Startin\' Somethin\'', 'Thriller', 'Beat It']
+  ],
+  [
+    52,
+    'Abbey Road',
+    'The Beatles',
+    'abbey_road.png',
+    '1969',
+    ['Come Together', 'Something', 'Here Comes the Sun']
+  ],
+  [
+    53,
+    'The Dark Side of the Moon',
+    'Pink Floyd',
+    'dark_side_of_the_moon.png',
+    '1973',
+    ['Money', 'Time', 'The Great Gig in the Sky']
+  ],
+  [
+    54,
+    'Hotel California',
+    'Eagles',
+    'hotel_california.png',
+    '1976',
+    ['Hotel California', 'New Kid in Town', 'Life in the Fast Lane']
+  ],
+  [
+    55,
+    'Rumours',
+    'Fleetwood Mac',
+    'album.png',
+    '1977',
+    ['Dreams', 'Go Your Own Way', 'The Chain']
+  ],
+  [
+    56,
+    'Back in Black',
+    'AC/DC',
+    'album.png',
+    '1980',
+    ['Hells Bells', 'Back in Black', 'You Shook Me All Night Long']
+  ],
+  [
+    57,
+    'The Wall',
+    'Pink Floyd',
+    'album.png',
+    '1979',
+    ['Another Brick in the Wall, Part 2', 'Comfortably Numb', 'Run Like Hell']
+  ],
+  [
+    58,
+    'Sgt. Pepper\'s Lonely Hearts Club Band',
+    'The Beatles',
+    'album.png',
+    '1967',
+    ['Lucy in the Sky with Diamonds', 'A Day in the Life', 'With a Little Help from My Friends']
+  ],
+  [
+    59,
+    'Nevermind',
+    'Nirvana',
+    'album.png',
+    '1991',
+    ['Smells Like Teen Spirit', 'Come as You Are', 'Lithium']
+  ],
+  [
+    60,
+    'Led Zeppelin IV',
+    'Led Zeppelin',
+    'album.png',
+    '1971',
+    ['Stairway to Heaven', 'Black Dog', 'Rock and Roll']
+  ],
+];
